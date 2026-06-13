8d6ad195ad921dd6f0d945df941d63ab438f9f2c
Chirag <deshmukhchirag178@gmail.com>
2026-06-13 21:20:13 +0530

    cafe12_commit

---
+++ Cafe12/Cafe12/src/main/java/com/cafe12/Cafe12Application.java
@@ -0,0 +1,13 @@
+package com.cafe12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cafe12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cafe12Application.class, args);
+	}
+
+}
---
+++ Cafe12/Cafe12/src/test/java/com/cafe12/Cafe12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cafe12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
